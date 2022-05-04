46c7e95a09fd409b0175dbbf22a05372e089a135
GuilhermeAmarilho <guiamarilho1@outlook.com>
2022-05-04 06:44:37 -0300

    att diario

--- 3-semestre/Paradigmas-da-computação/Aula 06 - Projeto 1/assets/js/script.js
+++ 3-semestre/Paradigmas-da-computação/Aula 06 - Projeto 1/assets/js/script.js
@@ -1,14 +1,28 @@
-var button = document.querySelector('button');
 var bolin = document.querySelector('div.bolin');
+var vez = 0;
 var player1 = document.querySelectorAll('div.pessoas img')[0];
+player1.style.top = "-40vh";
+player1.style.left = "7vw";
 var player2 = document.querySelectorAll('div.pessoas img')[1];
+player2.style.top = "-40vh";
+player2.style.left = "7vw";
+var body = document.querySelector("body");
 
-function game(){
-    console.log("foi");
-}
+var str;
+var keycode;
 
-window.setInterval(game(), .5);
+body.addEventListener("keyup", function( e ) {
+    // console.log(e.key);
+    if(e.key == "a" || e.key == "ArrowLeft"){
+        if(vez%2==0){
+            player1.style.left = parseFloat(player1.style.left) - 0.2 + "vw";
+        }
+        if(vez%2==1){
+            player2.style.left = parseFloat(player1.style.left) - 0.2 + "vw";
+        }
+    }
+});
 
-bolin.style.top =  (Math.random() * (395 - 5) + 10)+"px";
-bolin.style.left =  (Math.random() * (790 - 460) + 460)+"px";
+bolin.style.top =  (Math.random() * (57 - 10) + 10)+"vh";
+bolin.style.left =  (Math.random() * (682 - 402) + 402)/10+"vw";
 
