6bbde76fa3b01654d85f619df344b41c800bce56
GuilhermeAmarilho <guiamarilho1@outlook.com>
2022-05-07 16:07:28 -0300

    att diario

---
+++ update-all.bat
@@ -0,0 +1,34 @@
+cd cadeiras
+git pull
+git add .
+git commit -m "att diario"
+git push
+cd..
+
+cd Arquivos
+git pull
+git add .
+git commit -m "att diario"
+git push
+cd..
+
+cd EstruturaDeDados1
+git pull
+git add .
+git commit -m "att diario"
+git push
+cd..
+
+cd Mercado-Amarilho
+git pull
+git add .
+git commit -m "att diario"
+git push
+cd..
+
+cd Projetos
+git pull
+git add .
+git commit -m "att diario"
+git push
+cd..
