c42c7a0a0784981c643623c1fbbf6a27685e9c17
guilherme amarilho <guiamarilho1@outlook.com>
2022-07-05 22:38:25 -0300

    att diario

--- 3-semestre/Paradigmas-da-computação/Aula11- Projeto3/Projeto3/Projeto3.rkt
+++ 3-semestre/Paradigmas-da-computação/Aula11- Projeto3/Projeto3/Projeto3.rkt
@@ -25,6 +25,22 @@
 
 (require 2htdp/batch-io) 
 (define frame (new frame% [label "Cálculo de bhaskara"][height 200][width 100]))
+
+(new message% [parent frame][label "Ultimos valores inseridos"])
+(define ha (new text-field% [label "A: "] [parent frame] [vert-margin 10] [horiz-margin 200]))
+(define hb (new text-field% [label "B: "] [parent frame] [vert-margin 10] [horiz-margin 200]))
+(define hc (new text-field% [label "C: "] [parent frame] [vert-margin 10] [horiz-margin 200]))
+
+(define hav (query-value pgc "select v.a from valores as v order by id DESC LIMIT 1"))
+(define hbv (query-value pgc "select v.b from valores as v order by id DESC LIMIT 1"))
+(define hcv (query-value pgc "select v.c from valores as v order by id DESC LIMIT 1"))
+
+(send ha set-value (number->string hav))
+(send hb set-value (number->string hbv))
+(send hc set-value (number->string hcv))
+
+
+(new message% [parent frame][label "Calcule uma nova baskara"])
 (new message% [parent frame][label "Bhaskara"])
 (define a (new text-field% [label "Coeficiente A"] [parent frame] [vert-margin 10] [horiz-margin 200] ))
 (define b (new text-field% [label "Coeficiente B"] [parent frame] [vert-margin 10] [horiz-margin 200]))
@@ -54,7 +70,9 @@
                          (define vx2 (new text-field% [label "X2:            "] [init-value ""] [parent frame] [vert-margin 10] [horiz-margin 200]))
                          ;Define o valor da variavel
                          (send Xv set-value (number->string (eixox(string->number (send a get-value)) (string->number (send b get-value)))))
+                         
                          (send Yv set-value (number->string (eixoy(string->number (send a get-value)) (string->number (send b get-value))(string->number (send c get-value)))))
+                         
                          (send vx1 set-value (number->string (raiz1(string->number (send a get-value)) (string->number (send b get-value))(string->number (send c get-value)))))
                          (send vx2 set-value (number->string (raiz2(string->number (send a get-value)) (string->number (send b get-value))(string->number (send c get-value)))))
                          ; Botão fechar
@@ -64,17 +82,6 @@
                                           (send frame show #f)                   
                               )])
                         )]))
-(new message% [parent frame][label "Ultimos valores inseridos"])
-(define ha (new text-field% [label "A: "] [parent frame] [vert-margin 10] [horiz-margin 200]))
-(define hb (new text-field% [label "B: "] [parent frame] [vert-margin 10] [horiz-margin 200]))
-(define hc (new text-field% [label "C: "] [parent frame] [vert-margin 10] [horiz-margin 200]))
-
-(define hav (query-value pgc "select v.a from valores as v order by id DESC LIMIT 1"))
-(define hbv (query-value pgc "select v.b from valores as v order by id DESC LIMIT 1"))
-(define hcv (query-value pgc "select v.c from valores as v order by id DESC LIMIT 1"))
-
-(send ha set-value (number->string hav))
-(send hb set-value (number->string hbv))
-(send hc set-value (number->string hcv))
 
 (send frame show #t)
+
--- 3-semestre/Paradigmas-da-computação/Aula11- Projeto3/Projeto3/banco.sql
+++ 3-semestre/Paradigmas-da-computação/Aula11- Projeto3/Projeto3/banco.sql
@@ -1,8 +1,16 @@
+DROP TABLE valores;
+
 CREATE TABLE valores(
     id serial primary key,
     a float,
     b float,
     c float
-)
+);
 
-insert into valores values (0,0,0,0)
+INSERT INTO valores VALUES (0,0,0,0);
+
+SELECT * FROM valores;
+
+SELECT * FROM valores ORDER BY id DESC;
+
+SELECT * FROM valores ORDER BY id DESC limit 1;
